b33d13cf852b023caff31503e58bb84406fa0b40
jorge27 <ja.garciavega@outlook.com>
2020-07-22 20:03:33 -0500

    Finish exceptions, iterations and control statements koans
    In this session, learn about how trigger a exception, about iterations,
    if's and some control statements

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,6 +14,17 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+	raise TriangleError if [a,b,c].min <= 0
+	x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
+  
+	if (a == b) && (b == c) && (a == c)
+		:equilateral
+	elsif ((a - b) == 0) || ((a - c) == 0) || ((b - c) == 0)
+		:isosceles
+	else
+	:scalene
+	end
   # WRITE THIS CODE
 end
 
